c60c255f2ea0276a018d0aee9cd916ee99a283cf
jmjac <jakub.m.jackowiak@gmail.com>
2017-03-27 20:25:09 +0200

    Added tests

--- tango_project/rango/models.py
+++ tango_project/rango/models.py
@@ -5,18 +5,18 @@ from django.template.defaultfilters import slugify
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     likes = models.IntegerField(default=0)
-    views = models.IntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
     slug = models.SlugField(unique=True)
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
 
-    class Meta:
-        verbose_name_plural = 'Categories'
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
 
 class Page(models.Model):
--- tango_project/rango/tests.py
+++ tango_project/rango/tests.py
@@ -1,1 +1,9 @@
 from django.test import TestCase
+from rango.models import Category
+
+
+class CategoryMethodTests(TestCase):
+    def test_slug_line_creation(self):
+        cat = Category(name='Random Name', views=0, likes=0)
+        cat.save()
+        self.assertEqual(cat.slug, 'random-name')
